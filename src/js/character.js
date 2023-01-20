cda588ddfede9767d4eb6c39c1114951b9509349
Ларина Марина <mlarina@domain.tkmistral.ru>
2023-01-20 12:04:15 +0300

    fixed error

--- src/js/character.js
+++ src/js/character.js
@@ -8,19 +8,16 @@ export default class Character {
       'Undead',
       'Zombie',
     ];
-  
     if (typeof name === 'string' && name.length >= 2 && name.length <= 10) {
       this.name = name;
     } else {
       throw new Error('Ошибка! Имя должно быть строкой, а также быть длиной от 2 до 10 символов!');
     }
-  
     if (!typesPerson.includes(type)) {
       throw new Error('Ошибка! Такого типа персонажа не существует!');
     } else {
       this.type = type;
     }
-  
     this.health = 100;
     this.level = 1;
   }
